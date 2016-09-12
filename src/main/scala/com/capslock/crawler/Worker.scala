c1a0410f401a3ae1f1643132b00aa69edd0609e9
capslock <capslock1874@gmail.com>
2016-09-12 22:52:53 +0800

    refactoring

--- src/main/scala/com/capslock/crawler/Worker.scala
+++ src/main/scala/com/capslock/crawler/Worker.scala
@@ -1,12 +1,13 @@
 package com.capslock.crawler
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor._
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Cookie
-import akka.http.scaladsl.model.{HttpHeader, HttpRequest, HttpResponse, StatusCodes}
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Materializer}
 
 import scala.collection.immutable
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 /**
@@ -23,7 +24,7 @@ class Worker(master: ActorRef)(implicit materializer: ActorMaterializer) extends
 
     override def receive: Receive = {
         case TaskResponse(url, body, headers, taskSender) =>
-            processResponse(url, body)
+            processResponse(url, body, headers)
             taskSender ! TaskResult(Some("worker finish"))
         case Task(_, url) =>
             val taskSender = sender()
@@ -40,16 +41,18 @@ class Worker(master: ActorRef)(implicit materializer: ActorMaterializer) extends
             }
     }
 
-    def processUserProfileResponse(username: String, url: String, body: String): Unit = {
+    def processUserProfileResponse(username: String, url: String, body: String, headers: immutable.Seq[HttpHeader]): Unit = {
         for (profile <- UserProfileExtractor(username, url, body).extract()) {
             println(profile.hash)
         }
     }
 
-    def processResponse(url: String, body: String): Unit = {
+    def processResponse(url: String, body: String, headers: immutable.Seq[HttpHeader]): Unit = {
         url match {
             case Worker.profileUrlPattern(username) =>
-                processUserProfileResponse(username, url, body)
+                processUserProfileResponse(username, url, body, headers)
+            case `url` if url.startsWith(Worker.profileFolloweesList) =>
+
         }
     }
 
@@ -62,10 +65,27 @@ class Worker(master: ActorRef)(implicit materializer: ActorMaterializer) extends
 
 object Worker {
     val cookie = Cookie(
+        ("a_t", "2.0AABALMEZAAAXAAAAF2b7VwAAQCzBGQAAAFDAl3xCoQkXAAAAYQJVTR061VcAPL0fuNcxkZIqUEH1aZN-jc1ztVuCuFIE1hL8SAOdlMihPr8nICgomA=="),
+        ("z_c0", "Mi4wQUFCQUxNRVpBQUFBSU1BTy1RaUZDaGNBQUFCaEFsVk5WWHI3VndBczJ5TlF3RDMwXzBpcU95UWJRdG9HWm5lU2dR|1473689929|ef67a4899d6c5ae5a6d4fd7a4e1f42a3f9fddcdb")
     )
     val profileUrlPattern = "https://www.zhihu.com/people/(.*)".r
+    val profileFolloweesList = "https://www.zhihu.com/node/ProfileFolloweesListV2"
 
     def props(master: ActorRef)(implicit materializer: Materializer): Props = {
         Props(classOf[Worker], master, materializer)
+    }
+
+    def main(args: Array[String]) {
+        implicit val system = ActorSystem("ClusterSystem")
+        implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system))
+        import ExecutionContext.Implicits.global
+        val http = Http(system)
+        val requestHeaders = immutable.Seq(Worker.cookie)
+        val offset = 0
+        val hash = "0970f947b898ecc0ec035f9126dd4e08"
+        val formData = FormData(("method", "next"), ("params", "{\"offset\":20,\"order_by\":\"created\",\"hash_id\":\"0970f947b898ecc0ec035f9126dd4e08\"}")).toEntity
+        val httpRequest = HttpRequest(HttpMethods.GET, "https://www.zhihu.com/node/ProfileFolloweesListV2", headers = requestHeaders, entity = formData)
+        http.singleRequest(httpRequest)
+            .foreach(response => println(response.headers.foreach(httpHeader => println(httpHeader))))
     }
 }
